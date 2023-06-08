2f7cd322f0231fb80d392f4751def35c21326c59
CymbalMonkey <xeniolum@mail.ru>
2023-06-08 15:14:49 +0300

    Пустой проект для последующего PR

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,47 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-
+        byte apples = 58;
+        System.out.println("Значение переменной apple c типом byte равно " + apples);
+        short hours = 25000;
+        System.out.println("Значение переменной hours с типом short равно " + hours);
+        int likes = 3675894;
+        System.out.println("Значение переменной likes с типом int равно " + likes);
+        long meters = 5;
+        System.out.println("Значение переменной meters с типом long равно " + meters);
+        float daysLeft = 19.745f;
+        System.out.println("Зачение переменной daysLeft с типом float равно " + daysLeft);
+        double kilograms = 35.293867;
+        System.out.println("Значение переменной kilograms с типом double равно " + kilograms);
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-
+        float a = 27.12f;
+        System.out.println(a);
+        long b = 987678965549L;
+        System.out.println(b);
+        double c = 2.786;
+        System.out.println(c);
+        short d = 569;
+        System.out.println(d);
+        short e = -159;
+        System.out.println(e);
+        int f = 27897;
+        System.out.println(f);
+        byte g = 67;
+        System.out.println(g);
     }
+
     public static void task3 () {
         System.out.println("Задача 3");
-
+        short studentsLP = 23;
+        short studentsAS = 27;
+        short studentsYeA = 30;
+        int totalStudents = studentsLP + studentsAS + studentsYeA;
+        short totalSheets = 480;
+        int sheetsPerStudent = totalSheets / totalStudents;
+        System.out.println("На каждого ученика рассчитано " + sheetsPerStudent + " листов бумаги");
     }
 
     public static void task4 () {
